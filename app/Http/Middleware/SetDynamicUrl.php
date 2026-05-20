82d1a4b7a967ae4bd9c388a179848318620c5a57
root <root@web2024>
2026-05-20 13:38:11 +0200

    Add SetDynamicUrl middleware, untrack generated view and cache files

---
+++ app/Http/Middleware/SetDynamicUrl.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\URL;
+use Symfony\Component\HttpFoundation\Response;
+
+class SetDynamicUrl
+{
+    public function handle(Request $request, Closure $next): Response
+    {
+        URL::forceRootUrl($request->getSchemeAndHttpHost());
+        return $next($request);
+    }
+}
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,6 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->trustProxies(at: '*');
+        $middleware->prepend(\App\Http\Middleware\SetDynamicUrl::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
